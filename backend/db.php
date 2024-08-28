a81e785497cdfbb150326893aafb9cb00f34bea0
Brang <brangtsawmaung89@gmail.com>
2024-08-28 18:06:37 +0630

    live backend

--- backend/db.php
+++ backend/db.php
@@ -3,15 +3,15 @@
 
 
 // Database connection parameters
-// $servername = "sql12.freesqldatabase.com";
-// $username = "sql12728284";
-// $password = "D5ucZQK3Vu";
-// $dbname = "sql12728284";
+$servername = "sql12.freesqldatabase.com";
+$username = "sql12728284";
+$password = "D5ucZQK3Vu";
+$dbname = "sql12728284";
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "hymns_myanmar";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "hymns_myanmar";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
