4373b34b588a329008ea58c4879572db86e2a275
Brang <brangtsawmaung89@gmail.com>
2024-08-30 23:42:49 +0630

    update pagination

--- backend/hymns.php
+++ backend/hymns.php
@@ -15,6 +15,12 @@ $sql = "
     LEFT JOIN categories ON hymns.category_id = categories.category_id
 ";
 
+$count_sql = "
+    SELECT COUNT(*) as total
+    FROM hymns
+    LEFT JOIN categories ON hymns.category_id = categories.category_id
+";
+
 $hasHymnName = isset($_GET['hymnname']) && !empty($_GET['hymnname']);
 
 if ($hasHymnName) {
@@ -24,6 +30,8 @@ if ($hasHymnName) {
     // Modify the SQL query to remove spaces from the title fields during the comparison
     $sql .= " WHERE REPLACE(hymns.burmeseTitle, ' ', '') LIKE '%$hymnname%' 
               OR REPLACE(hymns.englishTitle, ' ', '') LIKE '%$hymnname%'";
+    $count_sql .= " WHERE REPLACE(hymns.burmeseTitle, ' ', '') LIKE '%$hymnname%' 
+                    OR REPLACE(hymns.englishTitle, ' ', '') LIKE '%$hymnname%'";
 }
 
 // Pagination logic
@@ -31,23 +39,23 @@ $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
 $limit = 10; // Number of hymns per page
 $offset = ($page - 1) * $limit;
 
-// Add LIMIT to the SQL query
 $sql .= " LIMIT $limit";
 
 // Conditionally add OFFSET based on the presence of 'hymnname'
 if (!$hasHymnName) {
-    $offset = ($page - 1) * $limit;
     $sql .= " OFFSET $offset";
 }
 
-// Execute the query
+
+// Execute the queries
 $result = $conn->query($sql);
+$count_result = $conn->query($count_sql);
 
 $hymns_data = array();
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        $hymns_data[] = [
+        $hymns_data["hymns"][] = [
             'hymn_id' => $row['hymn_id'],
             'hymnNumber' => $row['hymnNumber'],
             'burmeseTitle' => $row['burmeseTitle'],
@@ -56,6 +64,15 @@ if ($result->num_rows > 0) {
             'bibleVerse' => $row['bibleVerse'],
             'categoryName' => $row['categoryName']
         ];
+    }
+
+    if($count_result && $count_result->num_rows > 0){
+        $count_row = $count_result->fetch_assoc();
+        $total_hymns = $count_row['total'];
+        // Calculate the total pages
+        $total_pages = ceil($total_hymns / $limit);
+        $hymns_data["total_pages"] = $total_pages;
+        $hymns_data["current_page"] = $page;
     }
 
     echo json_encode($hymns_data, JSON_UNESCAPED_UNICODE);
